3328177fabdc2100dc22c92ebeee17224307697b
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2023-05-30 19:07:32 -0300

    Adicionando telas

--- back-end/mysite/polls/urls.py
+++ back-end/mysite/polls/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('clientes', views.clientes, name='clientes'),
+    path('produtos', views.produtos, name='produtos'),
+    path('fornecedores', views.fornecedores, name='fornecedores'),
+    path('pedidos', views.pedidos, name='pedidos'),
+    path("login", views.login, name="login"),
+
 ]
--- back-end/mysite/polls/views.py
+++ back-end/mysite/polls/views.py
@@ -3,4 +3,28 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'teste.html', {"nome":"aaaa", "trails":["a","b","c"]})
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def clientes(request):
+    return render(request, 'clientes.html', {"nome":"Clientes", "tabela_Prop": ["ID","Nome","Email", "Telefone"],"dados":[
+        {"id":"1","nome":"Doe","email":"john@example.com","telefone":"(32)93123-8831",},
+        {"id":"2","nome":"Moe","email":"mary@example.com","telefone":"(12)93533-8512"},
+        {"id":"3","nome":"Dooley","email":"july@example.com","telefone":"(16)94223-3838"}]})
+
+def produtos(request):
+    return render(request, 'produtos.html', {"nome":"Produtos", "tabela_Prop": ["ID","Nome","Quantidade", "Preço"],"dados":[
+        {"id":"1","nome":"Pão","quantidade":"3","preco":"5,50",},
+        {"id":"2","nome":"Danone","quantidade":"5","preco":"2,90"},
+        {"id":"3","nome":"Bombom","quantidade":"7","preco":"1,50"}]})
+
+def fornecedores(request):
+    return render(request, 'fornecedores.html')
+
+def pedidos(request):
+    return render(request, 'pedidos.html')
+
+# def index(request):
+#     return render(request, 'teste.html', {"nome":"aaaa", "trails":["a","b","c", "d"]})
